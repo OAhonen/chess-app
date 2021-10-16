c7ca919dceed71e690dac1fe9e7e7012c527b571
OAhonen <olli.p.ahonen@tuni.fi>
2021-10-16 10:56:43 +0300

    Fix FetchPlayer

--- src/FetchPlayer.js
+++ src/FetchPlayer.js
@@ -2,16 +2,29 @@ import React, { useState } from 'react';
 import ShowPlayerInfo from './ShowPlayerInfo';
 
 function FetchPlayer() {
+  console.log('hello');
   const [player, setPlayer] = useState([]);
+  const [loading, isLoading] = useState(true)
 
   React.useEffect(() => {
-    fetch('https://api.chess.com/pub/player/ahone9/games/2021/04')
-      .then(response => response.json())
-      .then(data => setPlayer(data.games));
-  }, []);
+    const url = 'https://api.chess.com/pub/player/ahone9/games/2021/04';
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            setPlayer(json.games);
+            isLoading(false);
+        } catch (error) {
+            console.log("error", error);
+        }
+    };
+
+    fetchData();
+}, []);
 
   return (
-    <ShowPlayerInfo playerInfo={player}/>
+    loading ? <div>loading...</div> : <ShowPlayerInfo playerInfo={player}/>
   );
 }
 
