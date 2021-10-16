51500795c20d37579c790249cd711a2fb1d67864
OAhonen <olli.p.ahonen@tuni.fi>
2021-10-16 15:47:14 +0300

    Add more months to choose from

--- src/Form.js
+++ src/Form.js
@@ -6,7 +6,13 @@ function Form() {
   const [months] = useState([
     {label: '01', value: '01'},
     {label: '02', value: '02'},
-    {label: '03', value: '03'}
+    {label: '04', value: '03'},
+    {label: '05', value: '04'},
+    {label: '06', value: '05'},
+    {label: '07', value: '06'},
+    {label: '08', value: '07'},
+    {label: '09', value: '08'},
+    {label: '10', value: '09'}
   ]);
   const [month, setMonth] = useState("01");
   const [submitted, setSubmitted] = useState(false);
