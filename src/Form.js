b8eddb7bdb3ffb72d5c9f105e72e67c1ad5c63d5
OAhonen <olli.p.ahonen@tuni.fi>
2021-10-18 13:07:33 +0300

    Fix typo in months

--- src/Form.js
+++ src/Form.js
@@ -6,13 +6,14 @@ function Form() {
   const [months] = useState([
     {label: '01', value: '01'},
     {label: '02', value: '02'},
-    {label: '04', value: '03'},
-    {label: '05', value: '04'},
-    {label: '06', value: '05'},
-    {label: '07', value: '06'},
-    {label: '08', value: '07'},
-    {label: '09', value: '08'},
-    {label: '10', value: '09'}
+    {label: '03', value: '03'},
+    {label: '04', value: '04'},
+    {label: '05', value: '05'},
+    {label: '06', value: '06'},
+    {label: '07', value: '07'},
+    {label: '08', value: '08'},
+    {label: '09', value: '09'},
+    {label: '10', value: '10'}
   ]);
   const [month, setMonth] = useState("01");
   const [submitted, setSubmitted] = useState(false);
@@ -51,7 +52,6 @@ function Form() {
       }
 
     </div>
-    // {submitted ? <FetchPlayer name={name} month={month}/> : <div></div>}
   );
 }
 
