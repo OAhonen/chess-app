4159531d71c1a7e6cccd7db20b0946b01f596ce5
OAhonen <olli.p.ahonen@tuni.fi>
2021-11-02 12:51:40 +0200

    Display more data in table

--- src/ShowPlayerData.js
+++ src/ShowPlayerData.js
@@ -6,8 +6,16 @@ function ShowPlayerData(props) {
   let chessMoves = [];
   let gameCounter = 0;
   let winCounter = 0;
+  let drawCounter = 0;
+  let lossCounter = 0;
   let blackWinCounter = 0;
   let whiteWinCounter = 0;
+  let whiteGameCounter = 0;
+  let blackGameCounter = 0;
+  let whiteDrawCounter = 0;
+  let blackDrawCounter = 0;
+  let whiteLossCounter = 0;
+  let blackLossCounter = 0;
 
   function compareMoves() {
     playerInfo.forEach(e => {
@@ -32,13 +40,39 @@ function ShowPlayerData(props) {
     chessMoves.forEach(e => {
       if (e.sameMoves === true) {
         gameCounter++;
-        if (e.black.toLowerCase() === name && e.blackResult === 'win') {
-          winCounter++;
-          blackWinCounter++;
+        if (e.black.toLowerCase() === name) {
+          blackGameCounter++;
+          if (e.blackResult === 'win') {
+            winCounter++;
+            blackWinCounter++;
+          } else if (e.blackResult === 'checkmated' ||
+                    e.blackResult ==='timeout' ||
+                    e.blackResult === 'resigned' ||
+                    e.blackResult === 'lose' ||
+                    e.blackResult === 'abandoned') {
+            lossCounter++;
+            blackLossCounter++;
+          } else {
+            drawCounter++;
+            blackDrawCounter++;
+          }
         }
-        if (e.white.toLowerCase() === name && e.whiteResult === 'win') {
-          winCounter++;
-          whiteWinCounter++;
+        if (e.white.toLowerCase() === name) {
+          whiteGameCounter++;
+          if (e.whiteResult === 'win') {
+            winCounter++;
+            whiteWinCounter++;
+          } else if (e.whiteResult === 'checkmated' || 
+                    e.whiteResult === 'timeout' ||
+                    e.whiteResult === 'resigned' ||
+                    e.whiteResult === 'lose' ||
+                    e.whiteResult === 'abandoned') {
+            lossCounter++;
+            whiteLossCounter++;
+          } else {
+            drawCounter++;
+            whiteDrawCounter++;
+          }
         }
       }
     })
@@ -49,8 +83,40 @@ function ShowPlayerData(props) {
 
   return (
     <div>
-      Total games played: {gameCounter}<br/>
-      Wins: {winCounter} ({whiteWinCounter} as white) ({blackWinCounter} as black)
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>Total</th>
+            <th>White</th>
+            <th>Black</th>
+          </tr>
+          <tr>
+            <th>Games</th>
+            <td>{gameCounter}</td>
+            <td>{whiteGameCounter}</td>
+            <td>{blackGameCounter}</td>
+          </tr>
+          <tr>
+            <th>Wins</th>
+            <td>{winCounter}</td>
+            <td>{whiteWinCounter}</td>
+            <td>{blackWinCounter}</td>
+          </tr>
+          <tr>
+            <th>Draws</th>
+            <td>{drawCounter}</td>
+            <td>{whiteDrawCounter}</td>
+            <td>{blackDrawCounter}</td>
+          </tr>
+          <tr>
+            <th>Losses</th>
+            <td>{lossCounter}</td>
+            <td>{whiteLossCounter}</td>
+            <td>{blackLossCounter}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
